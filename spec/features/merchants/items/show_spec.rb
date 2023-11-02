b48e652eb7cf740dbc01f75eaf7219619ed4762b
Blaine Kennedy <blainekennedy@gmail.com>
2023-11-02 11:13:32 -0500

    remove any underscores

--- spec/features/merchants/items/show_spec.rb
+++ spec/features/merchants/items/show_spec.rb
@@ -2,25 +2,25 @@ require 'rails_helper'
 
 RSpec.describe 'Merchant Items Show Page', type: :feature do
   before (:each) do 
-    @merchant_1 = Merchant.create(name: 'Merchant 1')
-      @merchant_2 = Merchant.create(name: 'Merchant 2')
+    @merchant1 = Merchant.create(name: 'Merchant 1')
+    @merchant2 = Merchant.create(name: 'Merchant 2')
 
-      @item_1 = @merchant_1.items.create(name: 'Item 1', description: 'Description 1', unit_price: 1.00)
-      @item_2 = @merchant_1.items.create(name: 'Item 2', description: 'Description 2', unit_price: 2.00)
-      @item_3 = @merchant_2.items.create(name: 'Item 3', description: 'Description 3', unit_price: 3.00)
+    @item1 = @merchant1.items.create(name: 'Item 1', description: 'Description 1', unit_price: 1.00)
+    @item2 = @merchant1.items.create(name: 'Item 2', description: 'Description 2', unit_price: 2.00)
+    @item3 = @merchant2.items.create(name: 'Item 3', description: 'Description 3', unit_price: 3.00)
   end
   describe 'when a merchant clicks on an item name from the merchant items index page' do
     it 'displays the item details on the item show page' do
       
 
-      visit "/merchants/#{@merchant_1.id}/items"
-      click_link @item_1.name
+      visit "/merchants/#{@merchant1.id}/items"
+      click_link @item1.name
 
-      expect(current_path).to eq("/merchants/#{@merchant_1.id}/items/#{@item_1.id}")
+      expect(current_path).to eq("/merchants/#{@merchant1.id}/items/#{@item1.id}")
 
-      expect(page).to have_content(@item_1.name)
-      expect(page).to have_content(@item_1.description)
-      expect(page).to have_content(@item_1.unit_price)
+      expect(page).to have_content(@item1.name)
+      expect(page).to have_content(@item1.description)
+      expect(page).to have_content(@item1.unit_price)
     end
   end
 end
