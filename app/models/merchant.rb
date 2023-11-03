72f7ab51b7cadd4077b8e53846371c64c2bcf1ae
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-11-02 22:51:18 -0600

    Implement features to satisfy user story 3

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,15 +6,14 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def top_five_customers
-    hash = customers.reduce({}) do |transaction_hash, customer|
-      transaction_hash[customer] = Customer.find(customer.id).transactions.where(result: "success").count
-      transaction_hash
-    end
-    sorted_hash = hash.sort_by { |key, value| -value }[0..4]
-    require 'pry'; binding.pry
-    names_only = sorted_hash.map do |customer| 
-      "#{customer[0].first_name} #{customer[0].last_name}" 
-    end
-
+    # hash = customers.reduce({}) do |transaction_hash, customer|
+    #   transaction_hash[customer] = Customer.find(customer.id).transactions.where(result: "success").count
+    #   transaction_hash
+    # end
+    # sorted_hash = hash.sort_by { |key, value| -value }[0..4]
+    # names_only = sorted_hash.map do |customer| 
+    #   "#{customer[0].first_name} #{customer[0].last_name}" 
+    # end
+    Customer.joins(:items).joins(:transactions).select("customers.id, customers.first_name, customers.last_name, count(customers.id) as successful_transactions").where("items.merchant_id = ? and transactions.result = 0", self.id).group('customers.id').order(successful_transactions: :desc).limit(5)
   end
 end
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,79 +2,126 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :feature do 
   before(:each) do 
-    @merchant_list = create_list(:merchant , 1)
-    @item1 = create(:item, merchant: @merchant_list.sample)
-    @customer_list = create_list(:customer, 10)
-    @invoice_list = []
-    10.times do
-      @invoice_list << create(:invoice, customer: @customer_list.sample)
-    end
-    @invoice_item_list = []
-    10.times do
-      @invoice_item_list << create(:invoice_item, invoice: @invoice_list.sample, item: @item1)
-    end
-    @transaction_list = []
-    10.times do
-      @transaction_list << create(:transaction, invoice: @invoice_list.sample)
-    end
-    
 
-    # @item_1 = @merchant_1.create!(name: "Krabby Patty", description: "yummy", unit_price: "999")
+    
+    @merchant = create(:merchant)
+    @item1 = @merchant.items.create!(name: "Krabby Patty", description: "yummy", unit_price: "999")
 
   end
 
   describe "visiting the admin/namespace show page" do 
     describe "US1. When I visit my merchant dashboard" do
       it "Then I see the name of my merchant" do
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant.id}/dashboard"
 
-        expect(page).to have_content("Name: #{@merchant_list.first.name}")
+        expect(page).to have_content("Name: #{@merchant.name}")
       
       end
     end
 
     describe "US2. Then I see a link to my merchant items index" do
       it "And I see a link to my merchant invoices index" do
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant.id}/dashboard"
 
         expect(page).to have_link("Merchant Items Index")
         click_link("Merchant Items Index")
-        expect(current_path).to eq("/merchants/#{@merchant_list.first.id}/items")
+        expect(current_path).to eq("/merchants/#{@merchant.id}/items")
 
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant.id}/dashboard"
 
         expect(page).to have_link("Merchant Invoices Index")
         click_link("Merchant Invoices Index")
-        expect(current_path).to eq("/merchants/#{@merchant_list.first.id}/invoices")
+        expect(current_path).to eq("/merchants/#{@merchant.id}/invoices")
 
       end
     end
   end
   
   describe "US3. As a merchant, when I visit my merchant dashboard ('/merchants/:merchant_id/dashboard'" do
-    it "Then I see the names of my top 5 customers who have completed the largest number of successful transaction with my merchant" do
-      merchant = create(:merchant)
-      item = create(:item, merchant: merchant)
-      customer_list = create_list(:customer, 10)
-      invoice_list = []
-      customer_list.each do |customer|
-        invoice_list << create(:invoice, customer: customer)
-      end
-      invoice_item_list = []
-      invoice_list.each do |invoice|
-        invoice_item_list << create(:invoice_item, invoice: invoice, item: item)
-      end
-      transaction_list = []
-      1000.times do
-        transaction_list << create(:transaction, invoice: invoice_list.sample)
-      end
-      visit "/merchants/#{merchant.id}/dashboard"
-      require 'pry'; binding.pry
-    end
-
-    it "And next to each customer name I see the number of successful transactions they have conducted with my merchant" do
-      visit "/merchants/#{@merchant_list.first.id}/dashboard"
-      
+    describe "Then I see the names of my top 5 customers who have completed the largest number of successful transaction with my merchant" do
+      it "And next to each customer name I see the number of successful transactions they have conducted with my merchant" do
+        @merchant1 = Merchant.create(name: "Spongebob")
+        
+        @item1 = @merchant1.items.create(name: "Krabby Patty", description: "yummy", unit_price: "999")
+        @item2 = @merchant1.items.create(name: "Krabby Patty", description: "yummy", unit_price: "999")
+    
+        @customer1 = Customer.create(first_name: "Patrick", last_name: "Star")
+        @customer2 = Customer.create(first_name: "Sandy", last_name: "Cheeks")
+        @customer3 = Customer.create(first_name: "King", last_name: "Neptune")
+        @customer4 = Customer.create(first_name: "Eugene", last_name: "Krabs")
+        @customer5 = Customer.create(first_name: "Sheldon", last_name: "Plankton")
+        @customer6 = Customer.create(first_name: "Poppy", last_name: "Puff")
+    
+        @invoice1 = Invoice.create(status: 1, customer_id: @customer1.id)
+        @invoice2 = Invoice.create(status: 1, customer_id: @customer2.id)
+        @invoice3 = Invoice.create(status: 1, customer_id: @customer3.id)
+        @invoice4 = Invoice.create(status: 1, customer_id: @customer4.id)
+        @invoice5 = Invoice.create(status: 1, customer_id: @customer5.id)
+        @invoice6 = Invoice.create(status: 1, customer_id: @customer6.id)
+    
+        @invoiceitem1 = InvoiceItem.create(invoice_id: @invoice1.id, item_id: @item1.id)
+        @invoiceitem2 = InvoiceItem.create(invoice_id: @invoice2.id, item_id: @item1.id)
+        @invoiceitem3 = InvoiceItem.create(invoice_id: @invoice3.id, item_id: @item1.id)
+        @invoiceitem4 = InvoiceItem.create(invoice_id: @invoice4.id, item_id: @item2.id)
+        @invoiceitem5 = InvoiceItem.create(invoice_id: @invoice5.id, item_id: @item1.id)
+        @invoiceitem6 = InvoiceItem.create(invoice_id: @invoice6.id, item_id: @item1.id)
+  
+          Transaction.create(invoice_id: @invoice1.id, result: 0)
+          Transaction.create(invoice_id: @invoice2.id, result: 0)
+          Transaction.create(invoice_id: @invoice2.id, result: 0)
+        3.times do
+          Transaction.create(invoice_id: @invoice3.id, result: 1)
+        end
+        4.times do
+          Transaction.create(invoice_id: @invoice4.id, result: 0)
+        end
+        5.times do
+          Transaction.create(invoice_id: @invoice5.id, result: 0)
+        end
+        5.times do
+          Transaction.create(invoice_id: @invoice5.id, result: 1)
+        end
+        6.times do
+          Transaction.create(invoice_id: @invoice6.id, result: 0)
+        end
+  
+        @merchant2 = Merchant.create(name: "Squidward")
+        @item3 = @merchant2.items.create(name: "Krabby Patty", description: "yummy", unit_price: "999")
+        @customer7 = Customer.create(first_name: "Larry", last_name: "Lobster")
+        @invoice7 = Invoice.create(status: 1, customer_id: @customer7.id)
+        @invoiceitem7 = InvoiceItem.create(invoice_id: @invoice7.id, item_id: @item3.id)
+        7.times do
+          Transaction.create(invoice_id: @invoice7.id, result: 0)
+        end
+  
+        visit "/merchants/#{@merchant1.id}/dashboard"
+  
+        expect(page).to have_content("Top 5 Customers, # of Transactions")
+        expect("Poppy Puff: 6").to appear_before("Sheldon Plankton: 5")
+        expect("Sheldon Plankton: 5").to appear_before("Eugene Krabs: 4")
+        expect("Eugene Krabs: 4").to appear_before("Sandy Cheeks: 2")
+        expect("Sandy Cheeks: 2").to appear_before("Patrick Star: 1")
+      end      
     end
   end
 end
+
+
+# merchant_list = create_list(:merchant , 10)
+# item_list = []
+# 30.times do
+#   item_list << create(:item, merchant: merchant_list.sample)
+# end
+# customer_list = create_list(:customer, 10)
+# invoice_list = []
+# 10.times do
+#   invoice_list << create(:invoice, customer: customer_list.sample)
+# end
+# invoice_item_list = []
+# 10.times do
+#   invoice_item_list << create(:invoice_item, invoice: invoice_list.sample, item: item_list.sample)
+# end
+# transaction_list = []
+# 100.times do
+#   transaction_list << create(:transaction, invoice: invoice_list.sample)
+# end
