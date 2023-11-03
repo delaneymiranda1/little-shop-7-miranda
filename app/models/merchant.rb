2292ad826896c848c650f825470d0a5cf1e41c15
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-11-02 23:21:16 -0600

    Pass test for US 24 model method

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -6,14 +6,10 @@ class Merchant < ApplicationRecord
   has_many :transactions, through: :invoices
 
   def top_five_customers
-    # hash = customers.reduce({}) do |transaction_hash, customer|
-    #   transaction_hash[customer] = Customer.find(customer.id).transactions.where(result: "success").count
-    #   transaction_hash
-    # end
-    # sorted_hash = hash.sort_by { |key, value| -value }[0..4]
-    # names_only = sorted_hash.map do |customer| 
-    #   "#{customer[0].first_name} #{customer[0].last_name}" 
-    # end
     Customer.joins(:items).joins(:transactions).select("customers.id, customers.first_name, customers.last_name, count(customers.id) as successful_transactions").where("items.merchant_id = ? and transactions.result = 0", self.id).group('customers.id').order(successful_transactions: :desc).limit(5)
+  end
+  
+  def self.top_five_customers
+    Customer.joins(:items).joins(:transactions).select("customers.id, customers.first_name, customers.last_name, count(customers.id) as successful_transactions").where("transactions.result = 0").group('customers.id').order(successful_transactions: :desc).limit(5)
   end
 end
