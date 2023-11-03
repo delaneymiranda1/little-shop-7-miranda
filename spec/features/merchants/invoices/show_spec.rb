8d142e4dd8340ca99a4805c36c5defb384fcf9be
Miranda Delaney <delaney.miranda1@gmail.com>
2023-11-02 21:32:19 -0600

    test: add another merchants invoice to test to ensure all invoices are really being shown

--- spec/features/merchants/invoices/show_spec.rb
+++ spec/features/merchants/invoices/show_spec.rb
@@ -3,20 +3,26 @@ require 'rails_helper'
 RSpec.describe "Merchants Invoices Show", type: :feature do 
   before(:each) do 
     @merchant1 = Merchant.create(name: "Spongebob")
+    @merchant2 = Merchant.create(name: "Plankton")
 
     @item1 = @merchant1.items.create(name: "Krabby Patty", description: "yummy", unit_price: "999")
     @item2 = @merchant1.items.create(name: "Diet Dr Kelp", description: "spicy", unit_price: "555")
     @item3 = @merchant1.items.create(name: "Pretty Pattie", description: "cute", unit_price: "333")
+    @item4 = @merchant2.items.create(name: "Chum Bucket", description: "chummy", unit_price: "111")
 
     @customer1 = Customer.create(first_name: "Patrick", last_name: "Star")
     @customer2 = Customer.create(first_name: "Sandy", last_name: "Cheeks")
+    @customer3 = Customer.create(first_name: "Misses", last_name: "Puff")
 
     @invoice1 = Invoice.create(status: 1, customer_id: @customer1.id)
     @invoice2 = Invoice.create(status: 1, customer_id: @customer2.id)
+    @invoice3 = Invoice.create(status: 1, customer_id: @customer3.id)
+
 
     @invoiceitem1 = InvoiceItem.create(quantity: 3, status: 1, invoice_id: @invoice1.id, item_id: @item1.id)
     @invoiceitem2 = InvoiceItem.create(quantity: 2, status: 1, invoice_id: @invoice2.id, item_id: @item2.id)
     @invoiceitem3 = InvoiceItem.create(quantity: 1, status: 1, invoice_id: @invoice1.id, item_id: @item3.id)
+    @invoiceitem4 = InvoiceItem.create(quantity: 4, status: 1, invoice_id: @invoice3.id, item_id: @item4.id)
   end
 
   describe "US15. When I visit my merchant's invoices show " do 
@@ -51,6 +57,7 @@ RSpec.describe "Merchants Invoices Show", type: :feature do
       expect(page).to have_content("#{@invoiceitem3.quantity}")
       expect(page).to have_content("#{@item3.unit_price}")
       expect(page).to have_content("#{@invoiceitem3.status}")
+
     end
   end
 end
