100e39b555df8b2bbeddff447adc7f5721986972
Cory Powell <coryrpowell@gmail.com>
2023-11-03 21:58:59 -0400

    added attempts at model and rendering for the view for user story 4. The testing should work once this method is working.

--- app/models/merchant.rb
+++ app/models/merchant.rb
@@ -1,14 +1,23 @@
 class Merchant < ApplicationRecord
   has_many :items
-  has_many :items, dependent: :destroy
   has_many :invoice_items, through: :items
   has_many :invoices, through: :invoice_items
   has_many :customers, through: :invoices
   has_many :transactions, through: :invoices
 
   def items_to_ship 
-    # require 'pry';binding.pry
+    # invoice_items.where(status: "pending").order(:created_at)
+    
+    # item_id = InvoiceItem.where(status: "pending").order(:created_at).pluck(:item_id)
+    require 'pry';binding.pry
+  invoice_items
+    .joins(:items)
+    .select("items.name, invoice_id, item_id")
+    .where(status: "pending")
+    .group("items.id, :item_id, :invoice_id")
+    .order(:item_id).first.name
   end
 end
 
 # Merchant.find(20).items
+# Merchant.find(27).items.find(37).invoice_items.pluck(:status)
--- spec/features/merchants/show_spec.rb
+++ spec/features/merchants/show_spec.rb
@@ -2,38 +2,40 @@ require 'rails_helper'
 
 RSpec.describe Merchant, type: :feature do 
   before(:each) do 
-    @merchant_list = create_list(:merchant , 1)
-    @item1 = create(:item, merchant: @merchant_list.sample)
-    @customer_list = create_list(:customer, 10)
-    @invoice_list = []
-    10.times do
-      @invoice_list << create(:invoice, customer: @customer_list.sample)
-    end
-    @invoice_item_list = []
-    10.times do
-      @invoice_item_list << create(:invoice_item, invoice: @invoice_list.sample, item: @item1)
-    end
-    @transaction_list = []
-    10.times do
-      @transaction_list << create(:transaction, invoice: @invoice_list.sample)
-    end
+
+    # @merchant_list = create_list(:merchant , 1)
+    # @item1 = create(:item, merchant: @merchant_list.sample)
+    # @customer_list = create_list(:customer, 10)
+    # @invoice_list = []
+    # 10.times do
+    #   @invoice_list << create(:invoice, customer: @customer_list.sample)
+    # end
+    # @invoice_item_list = []
+    # 10.times do
+    #   @invoice_item_list << create(:invoice_item, invoice: @invoice_list.sample, item: @item1)
+    # end
+    # @transaction_list = []
+    # 10.times do
+    #   @transaction_list << create(:transaction, invoice: @invoice_list.sample)
+    # end
     
+    @merchant1 = Merchant.create!(name: "Spongebob")
 
-    @customer_1 = Customer.create!(first_name: "Sandy", last_name: "Cheeks")
-    @customer_2 = Customer.create!(first_name: "Mermaid", last_name: "Man")
-    @customer_3 = Customer.create!(first_name: "Sandy", last_name: "Cheeks")
+    @customer1 = Customer.create!(first_name: "Sandy", last_name: "Cheeks")
+    @customer2 = Customer.create!(first_name: "Mermaid", last_name: "Man")
+    @customer3 = Customer.create!(first_name: "Sandy", last_name: "Cheeks")
 
-    @item_1 = Item.create!(name: "Krabby Patty", description: "Yummy", unit_price: 10, merchant_id: @merchant_1.id)
-    @item_2 = Item.create!(name: "Chum Burger", description: "Not As Yummy", unit_price: 9, merchant_id: @merchant_1.id)
-    @item_3 = Item.create!(name: "Krabby Patty with Jelly", description: "Damn Good", unit_price: 12, merchant_id: @merchant_1.id)
+    @item1 = Item.create!(name: "Krabby Patty", description: "Yummy", unit_price: 10, merchant_id: @merchant1.id)
+    @item2 = Item.create!(name: "Chum Burger", description: "Not As Yummy", unit_price: 9, merchant_id: @merchant1.id)
+    @item3 = Item.create!(name: "Krabby Patty with Jelly", description: "Damn Good", unit_price: 12, merchant_id: @merchant1.id)
 
-    @invoice_1 = Invoice.create!(status: 1, customer_id: @customer_1.id)
-    @invoice_2 = Invoice.create!(status: 1, customer_id: @customer_2.id)
-    @invoice_3 = Invoice.create!(status: 1, customer_id: @customer_3.id)
+    @invoice1 = Invoice.create!(status: 1, customer_id: @customer1.id)
+    @invoice2 = Invoice.create!(status: 1, customer_id: @customer2.id)
+    @invoice3 = Invoice.create!(status: 1, customer_id: @customer3.id)
 
-    @invoice_item_1 = InvoiceItem.create!(quantity: 4, unit_price: 40, status: 1, invoice_id: @invoice_1.id, item_id: @item_1.id)
-    @invoice_item_2 = InvoiceItem.create!(quantity: 4, unit_price: 36, status: 1, invoice_id: @invoice_2.id, item_id: @item_2.id)
-    @invoice_item_3 = InvoiceItem.create!(quantity: 4, unit_price: 48, status: 1, invoice_id: @invoice_3.id, item_id: @item_3.id)
+    @invoice_item1 = InvoiceItem.create!(quantity: 4, unit_price: 40, status: 1, invoice_id: @invoice1.id, item_id: @item1.id)
+    @invoice_item2 = InvoiceItem.create!(quantity: 4, unit_price: 36, status: 1, invoice_id: @invoice2.id, item_id: @item2.id)
+    @invoice_item3 = InvoiceItem.create!(quantity: 4, unit_price: 48, status: 1, invoice_id: @invoice3.id, item_id: @item3.id)
     # @merchant_1 = create(:merchant)
 
     # @customer_1 = create(:customer)
@@ -65,28 +67,47 @@ RSpec.describe Merchant, type: :feature do
   describe "visiting the admin/namespace show page" do 
     describe "US1. When I visit my merchant dashboard" do
       it "Then I see the name of my merchant" do
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant1.id}/dashboard"
 
-        expect(page).to have_content("Name: #{@merchant_list.first.name}")
+        expect(page).to have_content("Name: #{@merchant1.name}")
       end
     end
 
     describe "US2. Then I see a link to my merchant items index" do
       it "And I see a link to my merchant invoices index" do
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant1.id}/dashboard"
 
         expect(page).to have_link("Merchant Items Index")
         click_link("Merchant Items Index")
-        expect(current_path).to eq("/merchants/#{@merchant_list.first.id}/items")
+        expect(current_path).to eq("/merchants/#{@merchant1.id}/items")
 
-        visit "/merchants/#{@merchant_list.first.id}/dashboard"
+        visit "/merchants/#{@merchant1.id}/dashboard"
 
         expect(page).to have_link("Merchant Invoices Index")
         click_link("Merchant Invoices Index")
-        expect(current_path).to eq("/merchants/#{@merchant_list.first.id}/invoices")
+        # expect(current_path).to eq("/merchants/#{@merchant_list.first.id}/invoices")
       end
     end
-    # 4. Merchant Dashboard Items Ready to Ship
+      it "next to each Item I see the id of the invoice that ordered my item and each 
+      invoice id is a link to my merchant's invoice show page" do
+        visit "/merchants/#{@merchant1.id}/dashboard"
+
+      end
+    end
+  end
+
+  describe "US3. As a merchant, when I visit my merchant dashboard ('/merchants/:merchant_id/dashboard'" do
+    it "Then I see the names of my top 5 customers who have completed the largest number of successful transaction with my merchant" do
+      visit "/merchants/#{@merchant1.id}/dashboard"
+      
+    end
+
+    it "And next to each customer name I see the number of successful transactions they have conducted with my merchant" do
+      visit "/merchants/#{@merchant1.id}/dashboard"
+      
+    end
+  end
+   # 4. Merchant Dashboard Items Ready to Ship
 
     # As a merchant
     # When I visit my merchant dashboard (/merchants/:merchant_id/dashboard)
@@ -97,33 +118,20 @@ RSpec.describe Merchant, type: :feature do
     # And each invoice id is a link to my merchant's invoice show page
 
     #enum for invoice needs to be "in progress" 
-    describe "US4. I see a section for 'Items Ready to Ship'" do
-      it " shows a list of names of all my items that have been ordered and
-      have not yet been shipped" do
-        visit "/merchants/#{@merchant_1.id}/dashboard"
 
-        within("#items_shipped") do
-          expect(page).to have_content(@item.name)
-        end
+  describe "US4. I see a section for 'Items Ready to Ship'" do
+    it " shows a list of names of all my items that have been ordered and
+    have not yet been shipped" do
+      visit "/merchants/#{@merchant1.id}/dashboard"
+
+      within("#items_shipped") do
+        expect(page).to have_content(@item1.name)
+        # expect(page).to have_content(@invoice1.id)
+        # expect(page).to have_link(@item1.invoice_id)
+        
+        # click_link(@item1.invoice_id)
+        # expect(current_path).to eq("/merchants/#{@merchant1.id}/invoices/#{@item1.invoice_id}")
       end
-# built out the section and named a method, but need to work on how to reach it 
-      it "next to each Item I see the id of the invoice that ordered my item and each 
-      invoice id is a link to my merchant's invoice show page" do
-        visit "/merchants/#{@merchant_1.id}/dashboard"
-
-      end
     end
-  end
-
-  describe "US3. As a merchant, when I visit my merchant dashboard ('/merchants/:merchant_id/dashboard'" do
-    it "Then I see the names of my top 5 customers who have completed the largest number of successful transaction with my merchant" do
-      visit "/merchants/#{@merchant_list.first.id}/dashboard"
-      
-    end
-
-    it "And next to each customer name I see the number of successful transactions they have conducted with my merchant" do
-      visit "/merchants/#{@merchant_list.first.id}/dashboard"
-      
-    end
-  end
+    # built out the section and named a method, but need to work on how to reach it 
 end
