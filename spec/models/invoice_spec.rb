7a9808e71041e1e8e74123ca6527ea5453a231d8
Miranda Delaney <delaney.miranda1@gmail.com>
2023-11-07 21:26:21 -0700

    test: add model testing for total revenue

--- spec/models/invoice_spec.rb
+++ spec/models/invoice_spec.rb
@@ -32,4 +32,26 @@ RSpec.describe Invoice, type: :model do
       end
     end
   end
+
+  describe "#total_revenue" do
+    it 'calculates the total revenue for invoice items ' do
+      merchant1 = Merchant.create(name: "Spongebob", enabled: true)
+      merchant2 = Merchant.create(name: "Plankton" , enabled: true)
+
+      item1 = merchant1.items.create(name: "Krabby Patty", description: "yummy", unit_price: "999")
+      item2 = merchant1.items.create(name: "Diet Dr Kelp", description: "spicy", unit_price: "555")
+
+      customer1 = Customer.create(first_name: "Patrick", last_name: "Star")
+      customer2 = Customer.create(first_name: "Sandy", last_name: "Cheeks")
+      
+      invoice1 = Invoice.create(status: 1, customer_id: customer1.id)
+      invoice2 = Invoice.create(status: 1, customer_id: customer2.id)
+      
+      invoiceitem1 = InvoiceItem.create(quantity: 3, unit_price: 999, status: 1, invoice_id: invoice1.id, item_id: item1.id)
+      invoiceitem2 = InvoiceItem.create(quantity: 2, unit_price: 555, status: 1, invoice_id: invoice2.id, item_id: item2.id)
+      
+      expect(invoice1.total_revenue).to eq(2997)
+      expect(invoice2.total_revenue).to eq(1110)
+    end
+  end
 end
