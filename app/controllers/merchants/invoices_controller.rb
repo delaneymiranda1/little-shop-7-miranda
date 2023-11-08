47bdc31cf529100ef70ddea8b3d6520489ad1446
Michael Kuhlmeier <kuhlmmb@gmail.com>
2023-11-07 20:44:56 -0700

    Finalize MVC Improvements

--- app/controllers/merchants/invoices_controller.rb
+++ app/controllers/merchants/invoices_controller.rb
@@ -2,21 +2,17 @@ class Merchants::InvoicesController < ApplicationController
     
   def index
     @merchant = Merchant.find(params[:merchant_id])
-    @invoices = @merchant.invoices.includes(:items)
   end
   
   def show
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = @merchant.invoices.find(params[:id])
-    @items = @invoice.items
-    @invoice_items = @invoice.invoice_items.includes(:item)
-    @total_revenue = @invoice.invoice_items.sum('unit_price * quantity')
   end
 
   def update
     @merchant = Merchant.find(params[:merchant_id])
     @invoice = @merchant.invoices.find(params[:id])
-    @invoice_items = @invoice.invoice_items.includes(:item)
+    @invoice_items = @invoice.invoice_items
     if @invoice_items.update(invoice_items_params)
       redirect_to "/merchants/#{@merchant.id}/invoices/#{@invoice.id}"
     else
@@ -24,6 +20,7 @@ class Merchants::InvoicesController < ApplicationController
     end
   end
 
+  private
   def invoice_items_params
     params.require(:invoice_item).permit(:quantity, :unit_price, :status)
   end
